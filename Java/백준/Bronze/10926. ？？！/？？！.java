f3b8749055c334823e8bae0a6c03e094710cbfd2
황민지 <139426988+hmj6589@users.noreply.github.com>
2024-02-22 19:12:03 +0900

    [Bronze V] Title: ??!, Time: 204 ms, Memory: 17696 KB -BaekjoonHub

---
+++ Java/백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner s=new Scanner(System.in);
+        
+        String a;
+        a=s.next();
+        
+        System.out.print(a+"??!");
+    }
+}
