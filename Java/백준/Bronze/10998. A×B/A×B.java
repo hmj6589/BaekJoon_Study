11a96403d1f0ab0f937c39ab61e5fe834fc87df2
황민지 <139426988+hmj6589@users.noreply.github.com>
2024-02-22 17:22:40 +0900

    [Bronze V] Title: A×B, Time: 208 ms, Memory: 17676 KB -BaekjoonHub

---
+++ Java/백준/Bronze/10998. A×B/A×B.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[]args){
+        Scanner s=new Scanner(System.in);
+        int a,b;
+        a=s.nextInt();
+        if(a<0){
+            a=s.nextInt();
+        }
+        b=s.nextInt();
+        if(b>10){
+            b=s.nextInt();
+        }
+        System.out.print(a*b);
+    }
+}
