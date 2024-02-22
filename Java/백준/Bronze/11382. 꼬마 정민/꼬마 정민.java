de89f592daea3b466b5a3d71967ff1cede6cd4ed
황민지 <139426988+hmj6589@users.noreply.github.com>
2024-02-22 20:50:04 +0900

    [Bronze V] Title: 꼬마 정민, Time: 208 ms, Memory: 17612 KB -BaekjoonHub

---
+++ Java/백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner s=new Scanner(System.in);
+        
+        long a,b,c;
+        a=s.nextLong();
+        b=s.nextLong();
+        c=s.nextLong();
+        
+
+        
+        System.out.println(a+b+c);
+    }
+}
