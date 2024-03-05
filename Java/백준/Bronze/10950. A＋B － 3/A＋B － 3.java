dac25bca38502f72cf7729c96cd8648144db9687
황민지 <139426988+hmj6589@users.noreply.github.com>
2024-03-05 23:33:29 +0900

    [Bronze V] Title: A+B - 3, Time: 232 ms, Memory: 17956 KB -BaekjoonHub

---
+++ Java/백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String [] args){
+        Scanner s=new Scanner(System.in);
+        
+        int t;
+        t=s.nextInt();
+        int v;
+        for(v=0;v<t;v++){
+            int a, b;
+            a=s.nextInt();
+            b=s.nextInt();
+            System.out.println(a+b);
+        }
+        
+        
+        
+        
+    }
+}
