8aefa14799943ecc54e4f060e7fc87e06cdc5848
황민지 <139426988+hmj6589@users.noreply.github.com>
2024-03-05 23:27:37 +0900

    [Bronze V] Title: 구구단, Time: 232 ms, Memory: 18512 KB -BaekjoonHub

---
+++ Java/백준/Bronze/2739. 구구단/구구단.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String [] args){
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        
+        int a;
+        for(a=1;a<10;a++){
+            System.out.println(n+" * "+a+" = "+n*a);
+        }
+        
+        
+    }
+}
